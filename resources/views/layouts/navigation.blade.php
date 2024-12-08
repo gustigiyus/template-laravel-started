4d2a04cf3b41fdb7ac0b380b901fe20f88faefdc
gustigiyus <gustiggt123@gmail.com>
2024-12-09 06:28:30 +0700

    Fix user profile pictures

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,7 +29,8 @@
                             aria-expanded="false" data-dropdown-toggle="dropdown-user">
                             <span class="sr-only">Open user menu</span>
                             <img class="w-8 h-8 rounded-full"
-                                src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
+                                src="{{ Auth::user()->profile_photo_path ? asset('storage/user-profile/' . Auth::user()->profile_photo_path) : '/images/default-avatar.png' }}"
+                                alt="user photo">
                         </button>
                     </div>
                     <div class="z-40 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
