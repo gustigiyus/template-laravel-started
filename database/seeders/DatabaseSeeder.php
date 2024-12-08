58226509b0c448e9262f11914212f7dada0ef419
gustigiyus <gustiggt123@gmail.com>
2024-12-09 06:20:01 +0700

    Fix database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Role;
 use App\Models\SettingApp;
 use App\Models\User;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,6 +18,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $startTime = Carbon::now();
+
         //? ROLES
         $adminSystemRole = Role::where('role_name', 'Admin System')->first();
 
@@ -25,18 +28,24 @@ class DatabaseSeeder extends Seeder
                 'id' => 1,
                 'role_name' => 'Admin System',
                 'role_desc' => 'Can access all features',
+                'created_at' => $startTime,
+                'updated_at' => $startTime,
             ]);
 
             DB::table('roles')->insert([
                 'id' => 2,
                 'role_name' => 'Admin Master',
                 'role_desc' => 'Can do specific features',
+                'created_at' => $startTime->copy()->addSeconds(1),
+                'updated_at' => $startTime->copy()->addSeconds(1),
             ]);
 
             DB::table('roles')->insert([
                 'id' => 3,
                 'role_name' => 'Admin',
                 'role_desc' => 'Can do basic stuff',
+                'created_at' => $startTime->copy()->addSeconds(2),
+                'updated_at' => $startTime->copy()->addSeconds(2),
             ]);
         }
 
@@ -46,36 +55,48 @@ class DatabaseSeeder extends Seeder
             'id' => 1,
             'menu_name' => 'Followup',
             'menu_desc' => 'User can acces crud menu followup',
+            'created_at' => $startTime,
+            'updated_at' => $startTime,
         ]);
 
         DB::table('menus')->insert([
             'id' => 2,
             'menu_name' => 'Cutting',
             'menu_desc' => 'User can acces crud menu cutting',
+            'created_at' => $startTime->copy()->addSeconds(1),
+            'updated_at' => $startTime->copy()->addSeconds(1),
         ]);
 
         DB::table('menus')->insert([
             'id' => 3,
             'menu_name' => 'Embro',
             'menu_desc' => 'User can acces crud menu embro',
+            'created_at' => $startTime->copy()->addSeconds(2),
+            'updated_at' => $startTime->copy()->addSeconds(2),
         ]);
 
         DB::table('menus')->insert([
             'id' => 4,
             'menu_name' => 'Sewing',
             'menu_desc' => 'User can access crud menu sewing',
+            'created_at' => $startTime->copy()->addSeconds(3),
+            'updated_at' => $startTime->copy()->addSeconds(3),
         ]);
 
         DB::table('menus')->insert([
             'id' => 5,
             'menu_name' => 'Delivery',
             'menu_desc' => 'User can access crud menu delivery',
+            'created_at' => $startTime->copy()->addSeconds(4),
+            'updated_at' => $startTime->copy()->addSeconds(4),
         ]);
 
         DB::table('menus')->insert([
             'id' => 6,
             'menu_name' => 'Payment',
             'menu_desc' => 'User can acces crud menu payment',
+            'created_at' => $startTime->copy()->addSeconds(5),
+            'updated_at' => $startTime->copy()->addSeconds(5),
         ]);
 
 
@@ -84,24 +105,36 @@ class DatabaseSeeder extends Seeder
             'username' => 'gustigiyus',
             'role_id' => 1,
             'password' => Hash::make(123456),
+            'email' => 'gustigiyus@gmail.com',
+            'created_at' => $startTime,
+            'updated_at' => $startTime,
         ]);
 
         $user2 = User::factory()->create([
             'username' => 'yuni',
             'role_id' => 2,
             'password' => Hash::make(123456),
+            'email' => 'yuni@gmail.com',
+            'created_at' => $startTime->copy()->addSeconds(1),
+            'updated_at' => $startTime->copy()->addSeconds(1),
         ]);
 
         $user3 = User::factory()->create([
             'username' => 'ida',
             'role_id' => 3,
             'password' => Hash::make(123456),
+            'email' => 'ida@gmail.com',
+            'created_at' => $startTime->copy()->addSeconds(2),
+            'updated_at' => $startTime->copy()->addSeconds(2),
         ]);
 
         $user4 = User::factory()->create([
             'username' => 'bagus',
             'role_id' => 3,
             'password' => Hash::make(123456),
+            'email' => 'bagus@gmail.com',
+            'created_at' => $startTime->copy()->addSeconds(3),
+            'updated_at' => $startTime->copy()->addSeconds(3),
         ]);
 
 
@@ -111,6 +144,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Gusti Giustianto',
             'nik' => 'UEB8342',
             'gender' => 'Male',
+            'dob' => '2000-04-25',
+            'address' => 'Tanjungsari, Sumedang',
+            'created_at' => $startTime,
+            'updated_at' => $startTime,
         ]);
 
         DB::table('user_details')->insert([
@@ -118,6 +155,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Yuni Rahayu',
             'nik' => 'URBGS84',
             'gender' => 'Female',
+            'dob' => '1989-05-11',
+            'address' => '',
+            'created_at' => $startTime->copy()->addSeconds(1),
+            'updated_at' => $startTime->copy()->addSeconds(1),
         ]);
 
         DB::table('user_details')->insert([
@@ -125,6 +166,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Ida Eliyanti',
             'nik' => 'NMQOPD8',
             'gender' => 'Female',
+            'dob' => '1994-04-20',
+            'address' => '',
+            'created_at' => $startTime->copy()->addSeconds(2),
+            'updated_at' => $startTime->copy()->addSeconds(2),
         ]);
 
         DB::table('user_details')->insert([
@@ -132,6 +177,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Bagus Indra',
             'nik' => 'PQJFV84',
             'gender' => 'Male',
+            'dob' => '1991-07-18',
+            'address' => '',
+            'created_at' => $startTime->copy()->addSeconds(3),
+            'updated_at' => $startTime->copy()->addSeconds(3),
         ]);
 
 
@@ -139,11 +188,15 @@ class DatabaseSeeder extends Seeder
         DB::table('brands')->insert([
             'brand_name' => 'Kadaka',
             'brand_desc' => 'Kadaka is the best brand',
+            'created_at' => $startTime,
+            'updated_at' => $startTime,
         ]);
 
         DB::table('brands')->insert([
             'brand_name' => 'Jogger',
             'brand_desc' => 'Jogger is the best brand',
+            'created_at' => $startTime->copy()->addSeconds(1),
+            'updated_at' => $startTime->copy()->addSeconds(1),
         ]);
 
 
@@ -151,6 +204,8 @@ class DatabaseSeeder extends Seeder
         DB::table('app_settings')->insert([
             'name_app' => 'App Name',
             'desc_app' => 'Apllication Description',
+            'created_at' => $startTime->copy()->addSeconds(2),
+            'updated_at' => $startTime->copy()->addSeconds(2),
         ]);
     }
 }
